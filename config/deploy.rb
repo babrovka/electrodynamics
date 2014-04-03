dfeb930f2b145bdf1b9882c40eceb2b4415ca836
babrovka <babrovka@gmail.com>
2014-04-03 12:14:50 +0400

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ ssh_options[:port] = 23813
 
 set :user, "babrovka"
 set :application, "electrodynamics"
-set :deploy_to, "/srv/webdata/#{application}"
+set :deploy_to, "/srv/webdata/electrodynamics.ru"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
@@ -36,20 +36,18 @@ set :branch, "master"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-
-
-# namespace :deploy do
-#   namespace :assets do
-#     task :precompile, :roles => :web, :except => { :no_release => true } do
-#       from = source.next_revision(current_revision)
-#       if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-#         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile --trace}
-#       else
-#         logger.info "Skipping asset pre-compilation because there were no asset changes"
-#       end
-#     end
-#   end
-# end
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile --trace}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
 
 namespace(:uwsgi) do
   task :restart do
