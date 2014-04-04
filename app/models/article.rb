05ed93df99b508ba7afb3244962b57c6c2283105
babrovka <babrovka@gmail.com>
2014-04-04 22:22:33 +0400

    .

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
-  attr_accessible :text, :title
+  attr_accessible :text, :title, :tag_ids
   
   has_many :article_tags
   has_many :tags, through: :article_tags
+  
+  belongs_to :author, class_name: 'User'
 end
