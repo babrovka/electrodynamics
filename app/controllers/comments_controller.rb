569c9cdf75fc7d6e9c78f54e7abbee41741bb2df
babrovka <babrovka@gmail.com>
2014-04-07 20:54:26 +0400

    commentscontroller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,21 @@
+# coding: utf-8
+
 class CommentsController < InheritedResources::Base
 
+  def create 
+    @comment = Comment.new(params[:comment])
+    if @comment.save
+      redirect_to :back, :notice => 'Спасибо за комментарий'
+    else
+      redirect_to :back
+    end
+  end
+  
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to :back, :notice => 'Ваш комментарий удален'
+  end
   
 end
