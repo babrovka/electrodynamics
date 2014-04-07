4d154f1c1b6983987c83863e92513de9a80380b5
babrovka <babrovka@gmail.com>
2014-04-07 20:42:23 +0400

    apphelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
+# coding: utf-8
+
 module ApplicationHelper
   
   def date_with_month(date)
     I18n.localize(date, :format => :with_month)
+  end
+  
+  def link_to_delete_comment(user, comment)
+    link_to 'удалить', comment_path(comment), :method => :delete, :confirm => 'Вы уверены?' if comment.author_id == user.id
   end
 end
