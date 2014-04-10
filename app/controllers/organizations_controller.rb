6f688f04793edfd399b7d3a5fc8fce22040cbdd1
babrovka <babrovka@gmail.com>
2014-04-10 19:03:02 +0400

    .

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,3 +1,8 @@
 class OrganizationsController < InheritedResources::Base
   before_filter :authenticate_user!, only: [:new, :create, :edit]
+  
+  def show
+    @organization = Organization.find(params[:id])
+    @articles = Article.all
+  end
 end
