2ebd1d6e1748f47aa7cf278aa0cc4914172cf176
babrovka <babrovka@gmail.com>
2014-04-14 16:14:29 +0400

    registrations controller & personal area

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :firstname, :lastname, :role, :nickname
+  attr_accessor :current_password
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :firstname, :lastname, :role, :nickname, :current_password
   
   ROLES = %w[moderator user]
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Electrodynamics::Application.routes.draw do
-  
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
+  resources :users, :only => [:index, :show] do
+    get 'personal', :on => :member
+  end
   resources :organizations
+  resources :comments
   resources :articles
   match '/news' => 'articles#news', as: :news
-  resources :comments
   root :to => 'articles#index'
   ActiveAdmin.routes(self)
 end
