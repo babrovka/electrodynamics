9d79eee40714696b5ea7dcb43cb186d9fe6b8fef
babrovka <babrovka@gmail.com>
2014-04-15 15:31:38 +0400

    tabs in personal area

--- app/models/notice_configuration.rb
+++ app/models/notice_configuration.rb
@@ -1,4 +1,5 @@
 class NoticeConfiguration < ActiveRecord::Base
-  attr_accessible :to_mailbox, :to_personal_messages, :about_new_messages, :bout_my_comment_reply, :about_my_reply_comments, :about_mention_me, :about_new_subscriber
+  attr_accessible :to_mailbox, :to_personal_messages, 
+                  :about_new_messages, :about_my_comment_reply, :about_my_reply_comments, :about_mention_me, :about_new_subscriber
   belongs_to :user
 end
