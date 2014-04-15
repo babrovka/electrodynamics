65be0a0b0805c22fa45ecc428d6933753e6b41f8
babrovka <babrovka@gmail.com>
2014-04-15 13:05:46 +0400

    attributes for notice configurations

--- app/models/notice_configuration.rb
+++ app/models/notice_configuration.rb
@@ -1,4 +1,4 @@
 class NoticeConfiguration < ActiveRecord::Base
-  attr_accessible :to_mailbox
+  attr_accessible :to_mailbox, :to_personal_messages, :about_new_messages, :bout_my_comment_reply, :about_my_reply_comments, :about_mention_me, :about_new_subscriber
   belongs_to :user
 end
