f4e6575b53fd44209715d0ada150a99dfac4cc3d
babrovka <babrovka@gmail.com>
2014-04-15 18:02:22 +0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
+Rake::Task['create:organizations'].invoke
 Rake::Task['create:users'].invoke
 Rake::Task['create:tags'].invoke
 Rake::Task['create:categories'].invoke
-Rake::Task['create:organizations'].invoke
 Rake::Task['create:articles'].invoke
 Rake::Task['create:countries_and_cities'].invoke
