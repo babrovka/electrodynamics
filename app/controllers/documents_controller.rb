4aa6a47f7d26c3dfd67373d97ebaf770759f0fd0
babrovka <babrovka@gmail.com>
2014-04-18 18:43:26 +0400

    document front-end

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,4 +2,11 @@ class DocumentsController < InheritedResources::Base
   def index 
     @documents = Document.all
   end
+  
+  def show 
+    @document = Document.find(params[:id])
+    @comment = Comment.new
+    @articles = Article.limit(10)
+  end
+  
 end
