236dabdf67182926c207e13d4cc731bfdf067a31
babrovka <babrovka@gmail.com>
2014-04-18 18:47:37 +0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,4 @@ Rake::Task['create:users'].invoke
 Rake::Task['create:tags'].invoke
 Rake::Task['create:categories'].invoke
 Rake::Task['create:articles'].invoke
-
+Rake::Task['create:documents'].invoke
