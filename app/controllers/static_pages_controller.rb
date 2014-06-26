6111858f9d94a760c1d5979f865fa1f5536826f9
babrovka <babrovka@gmail.com>
2014-06-26 17:24:02 +0400

    main panel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,6 @@ class StaticPagesController < ApplicationController
     @articles = Article.limit(5)
     @event = Article.offset(5).limit(8)
     @best = Article.offset(7).limit(5)
-    @tags = Tag.limit(9)
   end
 
 end
